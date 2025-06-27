6beda26ca3c18f03e8b545db003844ab9f3bd283
Prathamesh <pratham.dahakepsd@gmail.com>
2025-06-27 21:41:55 +0530

    Done with distanceCal

--- apps/js_apps/distance.js
+++ apps/js_apps/distance.js
@@ -5,15 +5,7 @@ let y2 = document.querySelector("#y2");
 
 let btn = document.querySelector("#calculate");
 
-function calculate() {
-  const x1X2 = (x2.value - x1.value) * (x2.value - x1.value);
-  const y1Y2 = (y2.value - y1.value) * (y2.value - y1.value);
-
-  const sqrt = Math.sqrt(x1X2 + y1Y2);
-  console.log(sqrt);
-}
-
-let allowedChars = "123456790.";
+let allowedChars = "1234567890.";
 
 let inputs = document.querySelectorAll("input");
 
@@ -33,4 +25,31 @@ inputs.forEach((inputField) => {
   });
 });
 
-btn.addEventListener("click", calculate);
+btn.addEventListener("click", () => {
+  let hasEmpty = true;
+  inputs.forEach((input) => {
+    if (input.value.trim() === "") {
+      hasEmpty = true;
+    } else {
+      hasEmpty = false;
+    }
+  });
+
+  if (hasEmpty) {
+    alert("Each co-ordinate must conntain values");
+  } else {
+    calculate();
+  }
+});
+
+function calculate() {
+  const x1X2 = (x2.value - x1.value) * (x2.value - x1.value);
+  const y1Y2 = (y2.value - y1.value) * (y2.value - y1.value);
+
+  const sqrt = Math.sqrt(x1X2 + y1Y2).toFixed(3);
+  console.log(sqrt);
+
+  let answer = document.querySelector("#answer");
+  answer.innerText = sqrt + " meters";
+  answer.classList.add("result");
+}
